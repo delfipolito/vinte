bad3f922d21151435786506feaa4d529723e9a7e
Delfina <delfinapolito@gmail.com>
2017-05-25 14:35:41 -0300

    our work section

--- js/home.js
+++ js/home.js
@@ -1,3 +1,66 @@
 $( document ).ready(function(){
   $(".button-collapse").sideNav();
+
+  $("#box1").mouseenter( function() {
+    $('#card1').animate({ bottom: "0"}, 500 );
+  });
+  $("#box1").mouseleave( function() {
+    $('#card1').animate({ bottom: "-30%"}, 500 );
+  });
+
+  $("#box2").mouseenter( function() {
+    $('#card2').animate({ bottom: "0"}, 500 );
+  });
+  $("#box2").mouseleave( function() {
+    $('#card2').animate({ bottom: "-30%"}, 500 );
+  });
+
+  $("#box3").mouseenter( function() {
+    $('#card3').animate({ bottom: "0"}, 500 );
+  });
+  $("#box3").mouseleave( function() {
+    $('#card3').animate({ bottom: "-30%"}, 500 );
+  });
+
+  $("#box4").mouseenter( function() {
+    $('#card4').animate({ bottom: "0"}, 500 );
+  });
+  $("#box4").mouseleave( function() {
+    $('#card4').animate({ bottom: "-30%"}, 500 );
+  });
+
+  $("#box5").mouseenter( function() {
+    $('#card5').animate({ bottom: "0"}, 500 );
+  });
+  $("#box5").mouseleave( function() {
+    $('#card5').animate({ bottom: "-30%"}, 500 );
+  });
+
+  $("#box6").mouseenter( function() {
+    $('#card6').animate({ bottom: "0"}, 500 );
+  });
+  $("#box6").mouseleave( function() {
+    $('#card6').animate({ bottom: "-30%"}, 500 );
+  });
+
+  $("#box7").mouseenter( function() {
+    $('#card7').animate({ bottom: "0"}, 500 );
+  });
+  $("#box7").mouseleave( function() {
+    $('#card7').animate({ bottom: "-30%"}, 500 );
+  });
+
+  $("#box8").mouseenter( function() {
+    $('#card8').animate({ bottom: "0"}, 500 );
+  });
+  $("#box8").mouseleave( function() {
+    $('#card8').animate({ bottom: "-30%"}, 500 );
+  });
+
+  $("#box9").mouseenter( function() {
+    $('#card9').animate({ bottom: "0"}, 500 );
+  });
+  $("#box9").mouseleave( function() {
+    $('#card9').animate({ bottom: "-30%"}, 500 );
+  });
 })
