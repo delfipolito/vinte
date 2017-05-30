3ac0c7fc475d76c1182f3e9c7f73a712baff334e
Delfina <delfinapolito@gmail.com>
2017-05-30 18:48:59 -0300

    meta tags

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
 <?php include_once("home.html"); ?>
+<html style="height: 100%;">
+  <head>
+    <title>Vinte Studio</title>
+    <meta name="description" content="Vinte is a professional website design and development studio based in Argentina. We create human experience in a digital world; using the best practices and latest web standards guidelines. The results are digital experiences lovingly hand coded by combining business strategy, identity, UX/UI, and content.">
+    <meta name="keywords" content="HTML,CSS,XML,JavaScript,web,development,developer,design,designer,HTML(5),CSS(3),SASS, jQuery,React.js,Ruby on Rails,brand,maria cabrera, delfina polito, vinte, studio, vinte studio">
+    <meta name="author" content="Delfina Polito">
+    <meta name="author" content="Maria Cabrera">
+  </head>
+</html>
